be46cae564330d9c46efda933c90a2797d5d5ef4
luluw <shashinmhrzn@gmail.com>
2024-02-13 22:33:11 +0545

    Changes to model & Added Train Loop

--- neuralnet/model.py
+++ neuralnet/model.py
@@ -2,14 +2,14 @@ import torch
 from torch import nn 
 
 class IntentModelClassifier(nn.Module):
-    def __init__(self, input_size, hidden_units, output_size):
+    def __init__(self, input_size, hidden_size, output_size):
         super(IntentModelClassifier, self).__init__()
         self.LinearLayer = nn.Sequential(
-                nn.Linear(input_size, hidden_units),
+                nn.Linear(input_size, hidden_size),
                 nn.ReLU(),
-                nn.Linear(hidden_units, hidden_units),
+                nn.Linear(hidden_size, hidden_size),
                 nn.ReLU(),
-                nn.Linear(hidden_units, output_size),
+                nn.Linear(hidden_size, output_size),
         )
 
     def forward(self, x):
