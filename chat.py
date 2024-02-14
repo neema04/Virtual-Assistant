444cbedeb5f5681a31462f1a90310abcc7fca009
luluw <shashinmhrzn@gmail.com>
2024-02-14 18:33:26 +0545

    [UPDATE] Terminal Layout, Packages and intents

--- chat.py
+++ chat.py
@@ -12,7 +12,7 @@ print(device)
 with open('intents.json', 'r') as json_data:
     intents = json.load(json_data)
 
-# Load Intent_Classifier Trained Model
+""" Load Intent_Classifier Trained Model """
 MODEL_SAVE_PATH = "model/intent.pth"
 model_info = torch.load(MODEL_SAVE_PATH)
 
@@ -25,15 +25,24 @@ model_state = model_info["model_state"]
 
 # Instantiate IntentModelClassifier
 model = IntentModelClassifier(
-    input_size, hidden_size, output_size).to(device)
+    input_size, 
+    hidden_size, 
+    output_size).to(device)
 
 model.load_state_dict(model_state)
 model.eval()
 
+
+# ANSI escape codes for colors
+GREEN = "\033[92m"
+RED = "\033[91m"
+RESET = "\033[0m"
+
+
 bot_name = "ByteBot"
 print("Let's chat! (type 'quit' to exit)")
 while True:
-    sentence = input("You: ")
+    sentence = input(f"{GREEN}You{RESET}: ")
     if sentence == "quit" or sentence == "bye":
         break
 
@@ -54,6 +63,6 @@ while True:
     if prob.item() > 0.75:
         for intent in intents['intents']:
             if tag == intent['tag']:
-                print(f"{bot_name}: {random.choice(intent['responses'])}")
+                print(f"{RED}{bot_name}{RESET}: {random.choice(intent['responses'])}")
     else:
-        print(f'{bot_name}: I donot understand...')
+        print(f'{RED}{bot_name}{RESET}: I donot understand...')
